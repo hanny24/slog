2ba539174e27b2ef79001d5370e831ebbf30fc66
Jan Strnad <strnad@avast.com>
2019-12-05 09:54:12 +0100

    example outputs

--- example/src/main/scala/example/Example.scala
+++ example/src/main/scala/example/Example.scala
@@ -10,13 +10,18 @@ class Example[F[_]](loggerFactory: LoggerFactory[F],
   // or
   // private [this] val logger = loggerFactory.make("my-logger")
 
-  // Every log message will use filename and line number as their context by default.
+  // example output json does not include timestamps, log levels, thread name,
+  // logger names etc to keep things simple. They are of course included in the real
+  // output.
   def foo: F[Unit] = {
     // simple log message. Might take from scope.
+    // {"message": "Hello world!!", "file": "Example.scala", "line": 19
     logger.info.msg("Hello world!!") >>
       // Provide extract context to the log message
+      // {"message": "Hello", "file": "Example.scala", "line": 22, "correlation_id": "<VALUE>"}
       logger.info.context("correlation_id", "<VALUE>").msg("Hello") >>
       // Provide extra context and an exception
+      // {"message": "Hello", "file": "Example.scala", "line": 25, "correlation_id": "<VALUE>", "stack_trace": "Exception ..."}
       logger.info
         .context("correlation_id", "<VALUE>")
         .msg(new Exception)("Hello") >>
@@ -24,9 +29,10 @@ class Example[F[_]](loggerFactory: LoggerFactory[F],
         .use("correlation_id", "<VALUE>")
         .use("request_id", "<VALUE>")
         .scoped {
-          // both of the log messages will use "correlation_id" and "request_id" as context information
-          logger.debug.msg(new RuntimeException)("test") >>
-            logger.info.msg("test")
+          // {"message": "test", "file": "Example.scala", "line": 33, "correlation_id": "<VALUE>", "request_id": "<VALUE>", "stack_trace": "Exception ..."}
+          logger.debug.msg(new Exception)("test") >>
+          // {"message": "test2", "file": "Example.scala", "line": 35, "correlation_id": "<VALUE>", "request_id": "<VALUE>"}
+          logger.info.msg("test2")
         }
   }
 
@@ -37,9 +43,10 @@ class Example[F[_]](loggerFactory: LoggerFactory[F],
     final case class Bar(values: List[Double])
     final case class Foo(value: Int, that: String, bar: Bar)
     import slog.generic.auto._
+    // {"message": "test", "file": "Example.scala", "line": 47, "foo_bar": {"value": 10, "that": "foobar", "bar": {"values": [42.0]}}}
     logger.trace
       .context("foo_bar", Foo(10, "foobar", Bar(List(42.0))))
-      .msg("Test")
+      .msg("test")
 
     // it's also easily possible to debug failed typeclass deriviation.
     // consider following example:
